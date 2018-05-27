90678e7f111bd6608c2b5e83c390858c98a7d2e9
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-27 15:21:03 +0200

    Correct exports.upload

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,9 +1,16 @@
-var fs = require('fs');
+var fs = require('fs'),
+    formidable = require('formidable');
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload");
-    response.write("Rozpoczynam upload!");
-    response.end()
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(erroe, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show'>");
+        response.end()
+    });
 }
 
 exports.welcome = function(request, response) {
